e65a54d303e115acc69a6c989f565ded3479e780
Zeynel Toplar <zt.2601@hotmail.com>
2021-07-21 20:24:50 +0300

    First-Commit

---
+++ src/app/models/car.ts
@@ -0,0 +1,9 @@
+export interface Car{
+    id:string;
+    brandName:string;
+    colorName:string;
+    carName:string;
+    modelYear:string;
+    dailyPrice:number;
+    description:string;
+}
---
+++ src/app/services/rental.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Rental } from '../models/rental';
+import { RentalResponseModel } from '../models/rentalResponseModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RentalService {
+
+  apiUrl:string="https://localhost:44394/api/rentals/getrentaldetails";
+
+  constructor(private httpClient:HttpClient) { }
+
+  getRentals():Observable<RentalResponseModel>{
+    return this.httpClient.get<RentalResponseModel>(this.apiUrl);
+  }
+}
